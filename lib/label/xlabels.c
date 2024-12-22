25521e2f2762a683eff506e4f222405e3a912b3b
Matthew Fernandez <matthew.fernandez@gmail.com>
2024-12-22 14:36:48 +1100

    label xladjust: abbreviate initialization of 'nbp'

--- lib/label/xlabels.c
+++ lib/label/xlabels.c
@@ -384,7 +384,6 @@ static BestPos_t xladjust(XLabels_t * xlp, object_t * objp)
     double xincr = (2 * lp->sz.x + objp->sz.x) / XLXDENOM;
     double yincr = (2 * lp->sz.y + objp->sz.y) / XLYDENOM;
     object_t *intrsx[XLNBR] = {0};
-    BestPos_t nbp;
 
     assert(objp->lbl);
 
@@ -397,7 +396,7 @@ static BestPos_t xladjust(XLabels_t * xlp, object_t * objp)
 	return bp;
     /*mid */
     lp->pos.y = objp->pos.y;
-    nbp = xlintersections(xlp, objp, intrsx);
+    BestPos_t nbp = xlintersections(xlp, objp, intrsx);
     if (nbp.n == 0)
 	return nbp;
     if (nbp.area < bp.area)
